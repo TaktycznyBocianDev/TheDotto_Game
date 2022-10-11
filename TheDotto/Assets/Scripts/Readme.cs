b0a506cdcb5b27acad17de266606748e074bc060
Stanisław Franczak <taktycznybociandev@gmail.com>
2022-10-11 22:57:22 +0200

    Commit
    This is commit after sortware issue

---
+++ TheDotto/Assets/Scripts/Readme.cs
@@ -0,0 +1,13 @@
+/* 
+ * The Dot Project
+ * 
+ * What I need to implement
+ * 1. Animations: Idle, Run, Attack, Smoking
+ * 2. Attack - 3 different spells; There I will use prototype pattern as in other project.
+ * 3. Trading - you can buy hp, mana potion and at every other level more power armor.
+ * 4. Enemies - 3/4 types??
+ * 5. Checking if player is on ground.
+ * 6. UI with hp/mana bars.
+ * 
+ * TODO: Start with 5, but also make new sprites for animation
+ */
